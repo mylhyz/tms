768e8709bdc8e496f311c6ab8b72a7148cec8438
mylhyz <mylhyz@gmail.com>
2024-07-17 09:03:13 +0800

    支持拉取模板仓库

--- tms.py
+++ tms.py
@@ -1,5 +1,42 @@
-import sys
+import os
 import argparse
+import subprocess
+
+TEMPLATE_REPO_URL = "https://gitlab.com/viper-repo/viper-templates.git"
+TEMPLATE_DIR = "templates"
+
+
+def exec_command(cmd, cwd):
+    proc = subprocess.Popen(
+        cmd.split(" "),
+        cwd=cwd,
+        shell=False,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    out, err = proc.communicate()
+    if err:
+        print(err)
+    else:
+        print(out.decode().strip())
+
+
+def get_project_dir():
+    return os.path.dirname(os.path.abspath(__file__))
+
+
+def get_template_dir():
+    return os.path.join(get_project_dir(), TEMPLATE_DIR)
+
+
+def clone_template():
+    cmd = "git clone {} {}".format(TEMPLATE_REPO_URL, get_template_dir())
+    exec_command(cmd, cwd=os.getcwd())
+
+
+def update_template():
+    cmd = "git pull --rebase"
+    exec_command(cmd, cwd=get_template_dir())
 
 
 def init(args):
@@ -10,10 +47,13 @@ def init(args):
 
 
 def upgrade(args):
-    if args.project_dir is None:
-        print("[UPGRADE]未指定 project_dir")
-    else:
-        print("[UPGRADE]指定 project_dir => {}".format(args.project_dir))
+    # 检查文件夹是否存在
+    template_dir = get_template_dir()
+    if not os.path.exists(template_dir):
+        print("开始克隆模板仓库")
+        clone_template()
+    # 拉取更新
+    update_template()
 
 
 def main():
@@ -27,10 +67,7 @@ def main():
         "project_dir", type=str, nargs="?", default=None, help="项目路径（可选）"
     )
     parser_init.set_defaults(func=init)
-    parser_upgrade = subparsers.add_parser("upgrade", help="升级项目模板")
-    parser_upgrade.add_argument(
-        "project_dir", type=str, nargs="?", default=None, help="项目路径（可选）"
-    )
+    parser_upgrade = subparsers.add_parser("upgrade", help="更新模板")
     parser_upgrade.set_defaults(func=upgrade)
 
     # 解析参数
