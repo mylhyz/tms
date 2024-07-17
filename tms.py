3c2653c447fc2bef1c3a4bbd540bdda6573b872b
mylhyz <mylhyz@gmail.com>
2024-07-17 10:03:05 +0800

    完成模板克隆

--- tms.py
+++ tms.py
@@ -1,9 +1,11 @@
 import os
+import sys
+import shutil
 import argparse
 import subprocess
 
 TEMPLATE_REPO_URL = "https://gitlab.com/viper-repo/viper-templates.git"
-TEMPLATE_DIR = "templates"
+TEMPLATE_REPO_DIR = "templates"
 
 
 def exec_command(cmd, cwd):
@@ -25,35 +27,72 @@ def get_project_dir():
     return os.path.dirname(os.path.abspath(__file__))
 
 
-def get_template_dir():
-    return os.path.join(get_project_dir(), TEMPLATE_DIR)
+def get_template_repo_dir():
+    return os.path.join(get_project_dir(), TEMPLATE_REPO_DIR)
 
 
-def clone_template():
-    cmd = "git clone {} {}".format(TEMPLATE_REPO_URL, get_template_dir())
+def clone_template_repo():
+    cmd = "git clone {} {}".format(TEMPLATE_REPO_URL, get_template_repo_dir())
     exec_command(cmd, cwd=os.getcwd())
 
 
-def update_template():
+def update_template_repo():
     cmd = "git pull --rebase"
-    exec_command(cmd, cwd=get_template_dir())
+    exec_command(cmd, cwd=get_template_repo_dir())
+
+
+def get_template_list():
+    for _, dirs, _ in os.walk(get_template_repo_dir()):
+        for dir in dirs:
+            if dir.startswith("."):
+                dirs.remove(dir)
+        return dirs
+
+
+def clone_template(dir, working):
+    print("[TMS] 开始克隆模板")
+    shutil.copytree(dir, working, dirs_exist_ok=True)
+    print("[TMS] 克隆模板完成")
 
 
 def init(args):
-    if args.project_dir is None:
-        print("[INIT]未指定 project_dir")
+    template_name = None
+    if args.t is None:
+        print("[TMS] 未使用 -t 指定项目模板")
+        sys.exit(1)
     else:
-        print("[INIT]指定 project_dir => {}".format(args.project_dir))
+        template_name = args.t
+        print("[TMS] 选择了 {} 模板".format(template_name))
+    working_dir = None
+    if args.project_dir is None:
+        working_dir = os.getcwd()
+        print("[TMS] 未指定 project_dir => {}".format(working_dir))
+    else:
+        working_dir = args.project_dir
+        print("[TMS] 指定 project_dir => {}".format(working_dir))
+    if working_dir is None or not os.path.exists(working_dir):
+        print("[TMS] {} 不存在".format(working_dir))
+        return
+    # 选择模板
+    template_dir = os.path.join(get_template_repo_dir(), template_name)
+    # 拷贝模板到指定目录下
+    clone_template(template_dir, working_dir)
 
 
 def upgrade(args):
     # 检查文件夹是否存在
-    template_dir = get_template_dir()
+    template_dir = get_template_repo_dir()
     if not os.path.exists(template_dir):
         print("开始克隆模板仓库")
-        clone_template()
+        clone_template_repo()
     # 拉取更新
-    update_template()
+    update_template_repo()
+
+
+def show(args):
+    dirs = get_template_list()
+    for dir in dirs:
+        print(dir)
 
 
 def main():
@@ -66,9 +105,12 @@ def main():
     parser_init.add_argument(
         "project_dir", type=str, nargs="?", default=None, help="项目路径（可选）"
     )
+    parser_init.add_argument("-t", type=str, nargs="?", help="选择模板")
     parser_init.set_defaults(func=init)
     parser_upgrade = subparsers.add_parser("upgrade", help="更新模板")
     parser_upgrade.set_defaults(func=upgrade)
+    parser_show = subparsers.add_parser("show", help="显示可用模板")
+    parser_show.set_defaults(func=show)
 
     # 解析参数
     args = parser.parse_args()
